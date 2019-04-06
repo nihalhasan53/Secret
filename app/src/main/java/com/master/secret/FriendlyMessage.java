192a286567f4d0b34d7ac263355841f9cd71d7b2
Nihal <nihalhasan53@gmail.com>
2019-04-06 22:16:35 +0530

    Final

---
+++ app/src/main/java/com/master/secret/FriendlyMessage.java
@@ -0,0 +1,23 @@
+package com.master.secret;
+
+public class FriendlyMessage {
+
+    private String name;
+
+    public FriendlyMessage() {
+    }
+
+    public FriendlyMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
